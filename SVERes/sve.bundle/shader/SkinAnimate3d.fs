785b0393dcb533ecea370abb0b6bf13ed61cce14
付一洲 <fuyizhou@svengine.com>
2018-10-09 14:27:53 +0800

    整理shader

--- SVERes/sve.bundle/shader/SkinAnimate3d.fs
+++
@@ -1,7 +0,0 @@
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-void main(){
-    gl_FragColor.rgba =texture2D(aTexture0,v_texcoord0);
-}
--- SVERes/sve.bundle/shader/geo3d.fs
+++
@@ -1,9 +0,0 @@
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-uniform vec4 u_color;
-
-void main(){
-    gl_FragColor = u_color;
-}
