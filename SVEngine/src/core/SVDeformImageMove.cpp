e3251c7ce84194c8af168852c1be8497891fe8bd
付一洲 <fuyizhou@svengine.com>
2018-10-12 19:43:16 +0800

    修改形变的空间

--- SVEngine/src/core/SVDeformImageMove.cpp
+++ SVEngine/src/core/SVDeformImageMove.cpp
@@ -32,8 +32,8 @@
 SVDeformImageMove::SVDeformImageMove(SVInst *_app)
 :SVGBase(_app){
     m_pMtlBg  = MakeSharedPtr<SVMtlCore>(mApp,"screennor");
-    m_pMeshBg = mApp->getRenderMgr()->createMeshRObj();
     m_pIUMP    = MakeSharedPtr<SVImageUsingMove>();
+    m_pMeshBg = nullptr;
     m_tt_w = 0;
     m_tt_h = 0;
     m_dataPoint = nullptr;
@@ -45,7 +45,7 @@ SVDeformImageMove::SVDeformImageMove(SVInst *_app)
 }
 
 SVDeformImageMove::~SVDeformImageMove(){
-    clearPass();
+    m_passPool.clear();
     m_pMeshBg = nullptr;
     m_pMtlBg = nullptr;
     m_pIUMP = nullptr;
@@ -65,23 +65,13 @@ void SVDeformImageMove::init(SVTexturePtr _intex,SVTexturePtr _texout){
                                                false);
         mApp->getRenderMgr()->pushRCmdCreate(m_fbo);
         //
-        SVPassPtr m_pass1 = MakeSharedPtr<SVPass>();
-        m_pass1->setInTex(0,_intex);
-        m_pass1->setOutTex(_texout);
-        m_pass1->setMtl(m_pMtlBg);
-        m_pass1->setMesh(m_pMeshBg);
-        addPass(m_pass1);
+        SVPassPtr t_pass = MakeSharedPtr<SVPass>();
+        t_pass->setMtl(m_pMtlBg);
+        t_pass->setMesh(m_pMeshBg);
+        t_pass->setInTex(0,_intex);
+        t_pass->setOutTex(_texout);
+        m_passPool.append(t_pass);
     }
-}
-
-void SVDeformImageMove::addPass(SVPassPtr _pass) {
-    if(_pass){
-        m_passPool.append(_pass);
-    }
-}
-
-void SVDeformImageMove::clearPass() {
-    m_passPool.clear();
 }
 
 void SVDeformImageMove::setPoint(V2 *_data){
@@ -96,46 +86,49 @@ void SVDeformImageMove::updatePointMSL(){
 }
 
 void SVDeformImageMove::_initPoint(){
-    if(m_pMeshBg) {
-        //索引数据
-        u16 m_dataIndex[14400];//30*80*2*3
-        //
-        s32 iWidthPoint = m_wPointCount , iHeightPoint = m_hPointCont;
-        s32 iDataCount = iWidthPoint * iHeightPoint;
-        s32 iWidthRect  = iWidthPoint - 1;
-        s32 iHeightRect = iHeightPoint - 1;
-        f32 fWidthDelta =  m_tt_w / iWidthRect;
-        f32 fHeightDelta = m_tt_h / iHeightRect;
-        s32 iDataIndex = 0;
-        memset(m_pointScreen , 0 , sizeof(V2) * iDataCount);
-        for (s32 i = 0 ; i < iHeightPoint ; ++i){
-            for (s32 j = 0 ; j < iWidthPoint ; ++j ){
-                m_pointScreen[iDataIndex].x = fWidthDelta * j;
-                m_pointScreen[iDataIndex].y = fHeightDelta * i;
-                iDataIndex += 1;
-            }
-        }
-        s32 iRectCount  = iWidthRect * iHeightRect;
-        s32 m_iIndexCount = iRectCount * 2 * 3;
-        s32 iIndexIndex = 0;
-        for (s32 i = 0 ; i < iHeightRect ; ++i){
-            for (s32 j = 0; j < iWidthRect ; ++j){
-                m_dataIndex[iIndexIndex]     = i * iWidthPoint + j;
-                m_dataIndex[iIndexIndex + 1] = i * iWidthPoint + j + 1;
-                m_dataIndex[iIndexIndex + 2] = (i + 1) * iWidthPoint + j;
-                m_dataIndex[iIndexIndex + 3] = (i + 1) * iWidthPoint + j;
-                m_dataIndex[iIndexIndex + 4] = i * iWidthPoint + j + 1;
-                m_dataIndex[iIndexIndex + 5] = (i + 1) * iWidthPoint + j + 1;
-                iIndexIndex += 6;
-            }
-        }
-        //第一次更新索引数据
-        SVDataSwapPtr t_index = MakeSharedPtr<SVDataSwap>();
-        t_index->writeData((void  *)m_dataIndex, m_iIndexCount * sizeof(u16));
-        m_pMeshBg->setIndexData(t_index, m_iIndexCount);
-        //第一次更新顶点数据
-        _refreshScreenRectMesh(m_pointScreen,m_pointScreen);
+    if(!m_pMeshBg) {
+        m_pMeshBg = mApp->getRenderMgr()->createMeshRObj();
     }
+    //索引数据
+    u16 m_dataIndex[14400];//30*80*2*3
+    //
+    s32 iWidthPoint = m_wPointCount , iHeightPoint = m_hPointCont;
+    s32 iDataCount = iWidthPoint * iHeightPoint;
+    s32 iWidthRect  = iWidthPoint - 1;
+    s32 iHeightRect = iHeightPoint - 1;
+    f32 fWidthDelta =  m_tt_w / iWidthRect;
+    f32 fHeightDelta = m_tt_h / iHeightRect;
+    s32 iDataIndex = 0;
+    memset(m_pointScreen , 0 , sizeof(V2) * iDataCount);
+    for (s32 i = 0 ; i < iHeightPoint ; ++i){
+        for (s32 j = 0 ; j < iWidthPoint ; ++j ){
+            m_pointScreen[iDataIndex].x = fWidthDelta * j;
+            m_pointScreen[iDataIndex].y = fHeightDelta * i;
+            iDataIndex += 1;
+        }
+    }
+    s32 iRectCount  = iWidthRect * iHeightRect;
+    s32 m_iIndexCount = iRectCount * 2 * 3;
+    s32 iIndexIndex = 0;
+    for (s32 i = 0 ; i < iHeightRect ; ++i){
+        for (s32 j = 0; j < iWidthRect ; ++j){
+            m_dataIndex[iIndexIndex]     = i * iWidthPoint + j;
+            m_dataIndex[iIndexIndex + 1] = i * iWidthPoint + j + 1;
+            m_dataIndex[iIndexIndex + 2] = (i + 1) * iWidthPoint + j;
+            m_dataIndex[iIndexIndex + 3] = (i + 1) * iWidthPoint + j;
+            m_dataIndex[iIndexIndex + 4] = i * iWidthPoint + j + 1;
+            m_dataIndex[iIndexIndex + 5] = (i + 1) * iWidthPoint + j + 1;
+            iIndexIndex += 6;
+        }
+    }
+    //第一次更新索引数据
+    SVDataSwapPtr t_index = MakeSharedPtr<SVDataSwap>();
+    t_index->writeData((void  *)m_dataIndex, m_iIndexCount * sizeof(u16));
+    m_pMeshBg->setIndexData(t_index, m_iIndexCount);
+    //第一次更新顶点数据
+    _refreshScreenRectMesh(m_pointScreen,m_pointScreen);
+    //创建mesh
+    m_pMeshBg->createMesh();
 }
 
 void SVDeformImageMove::_refreshScreenRectMesh(V2 *_data,V2 *_targetData){
@@ -161,16 +154,9 @@ void SVDeformImageMove::_refreshScreenRectMesh(V2 *_data,V2 *_targetData){
         t_datav->writeData(pVer, sizeof(V2_C_T0) * m_wPointCount * m_hPointCont);
         m_pMeshBg->setVertexDataNum( m_wPointCount * m_hPointCont );
         m_pMeshBg->setVertexData(t_datav);
-        m_pMeshBg->createMesh();
     }
 }
 
-//            SVPersonPtr t_person = mApp->getDetectMgr()->getPersonModule()->getPerson(1);
-//            if(m_dataPoint){
-//            }else if( t_person && t_person->getExist()){
-//                V2 *t_data = (V2*)t_person->getFaceDataOriginal();
-//                pointMove(t_data);
-//            }
 void SVDeformImageMove::update(f32 _dt){
     if(!is_swith){
         return;
@@ -187,23 +173,26 @@ void SVDeformImageMove::update(f32 _dt){
 }
 
 void SVDeformImageMove::render(){
-    if(!is_swith){
-        return;
-    }
-    if( !m_dataPoint ){
-        return;
-    }
     SVRenderScenePtr t_rs = mApp->getRenderMgr()->getRenderScene();
-    if (t_rs && false  == t_rs->isSuspend()) {
+    if (is_swith && t_rs && false  == t_rs->isSuspend() ) {
         for(s32 i=0;i<m_passPool.size();i++){
             if(m_passPool[i]->m_pMtl){
                 SVRenderCmdPassPtr t_cmd = MakeSharedPtr<SVRenderCmdPass>();
                 t_cmd->mTag = "SVBackGroundNode";
                 t_cmd->setFbo(m_fbo);
-                t_cmd->setTexture(m_passPool[i]->m_outTex);
-                t_cmd->setMesh(m_passPool[i]->m_pMesh);
-                t_cmd->setMaterial(m_passPool[i]->m_pMtl->clone());
-                t_rs->pushRenderCmd(RST_PREFILTER, t_cmd);
+                if( m_passPool[i]->m_outTexType == E_TEX_END ) {
+                    t_cmd->setTexture(m_passPool[i]->m_outTex);
+                }else{
+                    SVTexturePtr t_tex = mApp->getRenderer()->getSVTex(m_passPool[i]->m_outTexType);
+                    t_cmd->setTexture(t_tex);
+                }
+                if(m_passPool[i]->m_pMesh){
+                    t_cmd->setMesh(m_passPool[i]->m_pMesh);
+                }else{
+                    t_cmd->setMesh(mApp->getDataMgr()->m_screenMesh);
+                }
+                t_cmd->setMaterial(m_passPool[i]->m_pMtl);
+                t_rs->pushRenderCmd(RST_PREFILTER, t_cmd);//m_rsType
             }
         }
     }
@@ -289,3 +278,11 @@ void SVDeformImageMove::pointMove(V2 *t_data){
     //
     _refreshScreenRectMesh(m_pointScreen, t_targetData);
 }
+
+
+//            SVPersonPtr t_person = mApp->getDetectMgr()->getPersonModule()->getPerson(1);
+//            if(m_dataPoint){
+//            }else if( t_person && t_person->getExist()){
+//                V2 *t_data = (V2*)t_person->getFaceDataOriginal();
+//                pointMove(t_data);
+//            }
