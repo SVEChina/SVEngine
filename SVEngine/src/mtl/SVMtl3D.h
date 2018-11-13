5dc4c489e5fbf792039bc7b2ff567066047f6373
晓帆 <lixiaofan@svengine.com>
2018-11-13 14:34:28 +0800

    接入3D，效果不正确，待修正。

---
+++ SVEngine/src/mtl/SVMtl3D.h
@@ -0,0 +1,37 @@
+//
+// SVMtl3D.h
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#ifndef SV_MTL_3D_H
+#define SV_MTL_3D_H
+
+
+#include "SVMtlCore.h"
+
+//2D动画材质
+namespace sv {
+    
+    namespace mtl{
+        
+        class SVMtl3D : public SVMtlCore {
+        public:
+            SVMtl3D(SVInst *_app);
+            
+            SVMtl3D(SVMtl3D *_mtl);
+            
+            ~SVMtl3D();
+            
+            virtual void reset();
+            
+            virtual SVMtlCorePtr clone();
+        };
+
+    }//!namespace mtl
+    
+}//!namespace sv
+
+
+#endif //SV_MTL_3D_H
