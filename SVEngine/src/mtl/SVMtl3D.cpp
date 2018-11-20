b212addec3376a521dc454e101f9c8a80aef11e9
晓帆 <lixiaofan@svengine.com>
2018-11-20 17:57:02 +0800

    no message

--- SVEngine/src/mtl/SVMtl3D.cpp
+++ SVEngine/src/mtl/SVMtl3D.cpp
@@ -14,7 +14,7 @@ SVMtl3D::SVMtl3D(SVInst *_app, cptr8 _shader)
     m_LogicParamBlend.srcParam = GL_ONE;
     m_LogicParamBlend.dstParam = GL_ONE_MINUS_SRC_ALPHA;
     m_ambientStrength = 1.0f;
-    m_ambient_color.set(1.0f, 1.0f, 1.0f, 1.0f);
+    m_ambient_color.set(1.0f, 1.0f, 1.0f);
     for (s32 i=0; i<MAX_DIFFUSE_LIFHT*3; i++) {
         m_diffuseLightPosPool.append(0.0f);
     }
@@ -43,27 +43,26 @@ void SVMtl3D::reset() {
     SVMtlCore::reset();
 }
 
-void SVMtl3D::setDiffuseLightPos(u32 _channel, FVec3 _lightPos){
+void SVMtl3D::setDiffuseLight(u32 _channel, FVec3 _lightPos, FVec3 _lightColor){
     if (_channel >= MAX_DIFFUSE_LIFHT) {
         return;
     }
     m_diffuseLightPosPool[3*_channel + 0] = _lightPos.x;
     m_diffuseLightPosPool[3*_channel + 1] = _lightPos.y;
     m_diffuseLightPosPool[3*_channel + 2] = _lightPos.z;
-}
-
-void SVMtl3D::setDiffuseLightColor(u32 _channel, FVec3 _lightColor){
-    if (_channel >= MAX_DIFFUSE_LIFHT) {
-        return;
-    }
     m_diffuseLightColorPool[3*_channel + 0] = _lightColor.x;
     m_diffuseLightColorPool[3*_channel + 1] = _lightColor.y;
     m_diffuseLightColorPool[3*_channel + 2] = _lightColor.z;
 }
 
+void SVMtl3D::setAmbientLight(f32 _strength, FVec3 _ambientColor){
+    m_ambientStrength = _strength;
+    m_ambient_color = _ambientColor;
+}
+
 void SVMtl3D::_submitMtl(SVRendererBasePtr _render){
     _render->submitUniformf("u_ambientStrength", m_ambientStrength);
-    _render->submitUniformf4v("u_ambient_color", m_ambient_color.get());
+    _render->submitUniformf3v("u_ambient_color", m_ambient_color.get());
     _render->submitUniformf3v("u_diffuse_light_pos", m_diffuseLightPosPool.get(), MAX_DIFFUSE_LIFHT);
     _render->submitUniformf3v("u_diffuse_light_color", m_diffuseLightColorPool.get(), MAX_DIFFUSE_LIFHT);
 }
