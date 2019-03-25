55a27992ad1515c6336937ffa218ccef56b39788
付一洲 <fuyizhou@svengine.com>
2019-03-25 18:26:59 +0800

    增加相机控制相关2d,3d

--- SVEngine/src/act/SVNodeCtrlCamera.cpp
+++ SVEngine/src/act/SVNodeCtrlCamera.cpp
@@ -11,11 +11,41 @@
 
 SVNodeCtrlCamera::SVNodeCtrlCamera(SVInst* _app)
 :SVNodeCtrl(_app) {
-    
+    m_width = 720;
+    m_height = 1280;
+    m_dis = 100.0f;
 }
 
 SVNodeCtrlCamera::~SVNodeCtrlCamera() {
+    
 }
+
+//
+void SVNodeCtrlCamera::reset() {
+    
+}
+//
+void SVNodeCtrlCamera::reset(s32 _w,s32 _h) {
+    
+}
+//
+void SVNodeCtrlCamera::resize(s32 _w,s32 _h) {
+    
+}
+//平移 像素dert x,y
+void SVNodeCtrlCamera::move(s32 _px,s32 py) {
+    
+}
+//角度旋转 像素dert x,y
+void SVNodeCtrlCamera::angle(s32 _px,s32 py) {
+    
+}
+//推拉
+void SVNodeCtrlCamera::zoom(f32 _dis) {
+    
+}
+
+
 
 //相机控制
 void SVNodeCtrlCamera::run(SVCameraNodePtr _nodePtr, f32 dt) {
@@ -23,26 +53,12 @@ void SVNodeCtrlCamera::run(SVCameraNodePtr _nodePtr, f32 dt) {
     
 }
 
-//平移
-void SVNodeCtrlCamera::move(FVec3 _off) {
-    
-}
-
-//角度旋转
-void SVNodeCtrlCamera::angle(f32 _yaw,f32 _pitch) {
-    
-}
-
-//推拉
-void SVNodeCtrlCamera::zoom(f32 _dis) {
-    
-}
 
 /****************************************/
-SVCtrlCamera2D::SVCtrlCamera2D(SVInst* _app,s32 _width,s32 _height)
-:SVNodeCtrl(_app)
-,m_width(_width)
-,m_height(_height){
+SVCtrlCamera2D::SVCtrlCamera2D(SVInst* _app)
+:SVNodeCtrlCamera(_app){
+    m_width = 720;
+    m_height = 1280;
     m_dis = 100.0f;
     m_angle = 0.0f;
     m_target.set(0.0f);
@@ -52,17 +68,6 @@ SVCtrlCamera2D::SVCtrlCamera2D(SVInst* _app,s32 _width,s32 _height)
 }
 
 SVCtrlCamera2D::~SVCtrlCamera2D() {
-}
-
-//相机控制
-void SVCtrlCamera2D::run(SVCameraNodePtr _nodePtr, f32 dt) {
-    if(_nodePtr) {
-        if(m_dirty) {
-            _nodePtr->setTarget(m_target.x, m_target.y,m_target.z);
-            _nodePtr->setPosition(m_pos);
-            m_dirty = false;
-        }
-    }
 }
 
 //重制
@@ -81,16 +86,21 @@ void SVCtrlCamera2D::reset(s32 _w,s32 _h) {
     reset();
 }
 
+void SVCtrlCamera2D::resize(s32 _w,s32 _h) {
+    
+}
+
 //平移
-void SVCtrlCamera2D::move(FVec3 _off) {
-    m_target = m_target + _off;
-    m_pos = m_target;
-    m_pos.z += m_dis;
+void SVCtrlCamera2D::move(s32 _px,s32 py) {
     m_dirty = true;
+//    m_target = m_target + _off;
+//    m_pos = m_target;
+//    m_pos.z += m_dis;
+//
 }
 
 //角度旋转
-void SVCtrlCamera2D::angle(f32 _angle) {
+void SVCtrlCamera2D::angle(s32 _px,s32 py) {
     m_dirty = false;
 }
 
@@ -100,3 +110,13 @@ void SVCtrlCamera2D::zoom(f32 _dis) {
     m_dirty = true;
 }
 
+//相机控制
+void SVCtrlCamera2D::run(SVCameraNodePtr _nodePtr, f32 dt) {
+    if(_nodePtr) {
+        if(m_dirty) {
+            _nodePtr->setTarget(m_target.x, m_target.y,m_target.z);
+            _nodePtr->setPosition(m_pos);
+            m_dirty = false;
+        }
+    }
+}
