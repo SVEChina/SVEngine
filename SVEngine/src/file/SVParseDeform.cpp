e1e6abd9b949d8d5bae85410eb58f9c314382bdf
ziyu.xu <xuziyu@svengine.com>
2019-04-27 18:04:52 +0800

    修改变形和增加blur 滤镜

--- SVEngine/src/file/SVParseDeform.cpp
+++ SVEngine/src/file/SVParseDeform.cpp
@@ -7,8 +7,21 @@
 //
 
 #include "SVParseDeform.h"
+#include "../core/SVDeformImageMove.h"
+#include "../rendercore/SVRendererBase.h"
+#include "../rendercore/SVRenderTexture.h"
+#include "../rendercore/SVRenderMgr.h"
+#include "../app/SVInst.h"
 
-SVNodePtr SVParseDeform::parseDeform(SVInst *app, RAPIDJSON_NAMESPACE::Value &item, s32 _resid, cptr8 _path){
-    
-    return nullptr;
+
+SVDeformImageMovePtr SVParseDeform::parseDeform(SVInst *app, RAPIDJSON_NAMESPACE::Value &item, s32 _resid, cptr8 _path){
+    SVDeformImageMovePtr m_deform=MakeSharedPtr<SVDeformImageMove>(app);
+    SVTexturePtr t_innerTex = app->getRenderer()->getSVTex(E_TEX_MAIN);
+    m_deform->init(t_innerTex,t_innerTex);
+    m_deform->setIsDetect(true);
+    // m_deform->setvisiPoint(true);
+    m_deform->setFlip(true);
+    m_deform->m_rule=0;
+    m_deform->fromJSON(item);
+    return m_deform;
 }
--- SVEngine/src/file/SVParseDeform.h
+++ SVEngine/src/file/SVParseDeform.h
@@ -18,7 +18,7 @@ namespace sv {
         
         class SVParseDeform : public SVParseData {
         public:
-            static SVNodePtr parseDeform(SVInst *app, RAPIDJSON_NAMESPACE::Value &item, s32 _resid, cptr8 _path);
+            static SVDeformImageMovePtr parseDeform(SVInst *app, RAPIDJSON_NAMESPACE::Value &item, s32 _resid, cptr8 _path);
         };
         
     }//!namespace logic
