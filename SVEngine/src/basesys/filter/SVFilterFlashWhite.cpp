d5264e49935526e43a35c50d7e64ab70e89f2f03
晓帆 <lixiaofan@svengine.com>
2019-07-16 12:57:58 +0800

    添加内置纹理。

--- SVEngine/src/basesys/filter/SVFilterFlashWhite.cpp
+++ SVEngine/src/basesys/filter/SVFilterFlashWhite.cpp
@@ -34,7 +34,7 @@ bool SVFilterFlashWhite::create(){
     SVTexturePtr t_tex = t_renderer->getSVTex(E_TEX_MAIN);
     s32 t_w = t_tex->getwidth();
     s32 t_h = t_tex->getheight();
-    t_renderer->createSVTex(E_TEX_FILTER_2, t_w, t_h, GL_RGBA);
+    t_renderer->createSVTex(E_TEX_FILTER_FLASHWHITE, t_w, t_h, GL_RGBA);
     //创建多passnode
     m_pPassNode = MakeSharedPtr<SVMultPassNode>(mApp);
     m_pPassNode->setname("SVFilterFlashWhiteNode");
@@ -46,14 +46,14 @@ bool SVFilterFlashWhite::create(){
     m_mtlFlashWhite->setTexcoordFlip(1.0f, 1.0f);
     t_pass1->setMtl(m_mtlFlashWhite);
     t_pass1->setInTex(0,E_TEX_MAIN);
-    t_pass1->setOutTex(E_TEX_FILTER_2);
+    t_pass1->setOutTex(E_TEX_FILTER_FLASHWHITE);
     m_pPassNode->addPass(t_pass1);
 
     SVPassPtr t_pass2 = MakeSharedPtr<SVPass>();
     SVMtlCorePtr t_mtl_back= MakeSharedPtr<SVMtlCore>(mApp,"screennor");
     t_mtl_back->setTexcoordFlip(1.0f, 1.0f);
     t_pass2->setMtl(t_mtl_back);
-    t_pass2->setInTex(0, E_TEX_FILTER_2);
+    t_pass2->setInTex(0, E_TEX_FILTER_FLASHWHITE);
     t_pass2->setOutTex(E_TEX_MAIN);
     m_pPassNode->addPass(t_pass2);
     return true;
@@ -66,7 +66,7 @@ void SVFilterFlashWhite::destroy(){
     }
     SVRendererBasePtr t_renderer = mApp->getRenderer();
     if(t_renderer){
-        t_renderer->destroySVTex(E_TEX_FILTER_2);
+        t_renderer->destroySVTex(E_TEX_FILTER_FLASHWHITE);
     }
 }
 
--- SVEngine/src/basesys/filter/SVFilterShake.cpp
+++ SVEngine/src/basesys/filter/SVFilterShake.cpp
@@ -35,7 +35,7 @@ bool SVFilterShake::create(){
     SVTexturePtr t_tex = t_renderer->getSVTex(E_TEX_MAIN);
     s32 t_w = t_tex->getwidth();
     s32 t_h = t_tex->getheight();
-    t_renderer->createSVTex(E_TEX_FILTER_1, t_w, t_h, GL_RGBA);
+    t_renderer->createSVTex(E_TEX_FILTER_SHAKE, t_w, t_h, GL_RGBA);
     //创建多passnode
     m_pPassNode = MakeSharedPtr<SVMultPassNode>(mApp);
     m_pPassNode->setname("SVFilterShakeNode");
@@ -47,14 +47,14 @@ bool SVFilterShake::create(){
     m_mtlShake->setTexcoordFlip(1.0f, 1.0f);
     t_pass1->setMtl(m_mtlShake);
     t_pass1->setInTex(0,E_TEX_MAIN);
-    t_pass1->setOutTex(E_TEX_FILTER_1);
+    t_pass1->setOutTex(E_TEX_FILTER_SHAKE);
     m_pPassNode->addPass(t_pass1);
 
     SVPassPtr t_pass2 = MakeSharedPtr<SVPass>();
     SVMtlCorePtr t_mtl_back= MakeSharedPtr<SVMtlCore>(mApp,"screennor");
     t_mtl_back->setTexcoordFlip(1.0f, 1.0f);
     t_pass2->setMtl(t_mtl_back);
-    t_pass2->setInTex(0, E_TEX_FILTER_1);
+    t_pass2->setInTex(0, E_TEX_FILTER_SHAKE);
     t_pass2->setOutTex(E_TEX_MAIN);
     m_pPassNode->addPass(t_pass2);
     return true;
@@ -67,7 +67,7 @@ void SVFilterShake::destroy(){
     }
     SVRendererBasePtr t_renderer = mApp->getRenderer();
     if(t_renderer){
-        t_renderer->destroySVTex(E_TEX_FILTER_1);
+        t_renderer->destroySVTex(E_TEX_FILTER_SHAKE);
     }
 }
 
