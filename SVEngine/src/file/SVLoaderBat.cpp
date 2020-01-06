c51a268f1122c8cef657d8c800f3e23ce580020a
付一洲 <fuyizhou@svengine.com>
2020-01-06 18:01:46 +0800

    增加加载bat文件的类

---
+++ SVEngine/src/file/SVLoaderBat.cpp
@@ -0,0 +1,12 @@
+//
+// SVLoaderBat.cpp
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#include "SVLoaderBat.h"
+
+SVLoaderBat::SVLoaderBat(SVInst *_app)
+:SVFileLoader(_app) {
+}
---
+++ SVEngine/src/file/SVLoaderBat.h
@@ -0,0 +1,26 @@
+//
+// SVLoaderBat.h
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#ifndef SV_BATLOADER_H
+#define SV_BATLOADER_H
+
+#include "SVFileLoader.h"
+
+namespace sv {
+    
+    //加载普通的明文文件
+    class SVLoaderBat : public SVFileLoader {
+    public:
+        SVLoaderBat(SVInst *_app);
+        
+    };
+    
+}//!namespace sv
+
+
+
+#endif //SV_BATLOADER_H
